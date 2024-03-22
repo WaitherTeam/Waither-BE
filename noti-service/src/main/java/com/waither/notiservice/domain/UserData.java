f5ecedd1d5622d9fbd24420a4c22fcfa2295307d
DDonghyeo <gsu9045000@gmail.com>
2024-03-23 00:09:01 +0900

    ♻️refactor : User Data Default

--- noti-service/src/main/java/com/waither/notiservice/domain/UserData.java
+++ noti-service/src/main/java/com/waither/notiservice/domain/UserData.java
@@ -1,15 +1,19 @@
 package com.waither.notiservice.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Builder
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "user_data")
+@DynamicInsert
 @Entity
 public class UserData {
 
@@ -17,10 +21,19 @@ public class UserData {
     private Long userId;
 
     // 각 답변의 평균 값 (level 1 쪽이 추움 ~ level 5 쪽이 더움)
+    @ColumnDefault(value = "34")
     private Double level1;
+
+    @ColumnDefault(value = "28")
     private Double level2;
+
+    @ColumnDefault(value = "24")
     private Double level3;
+
+    @ColumnDefault(value = "10")
     private Double level4;
+
+    @ColumnDefault(value = "7")
     private Double level5;
 
     public void setLevel(int level, Double value) {
