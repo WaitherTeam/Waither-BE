b15a33e9f3153c98c919e586493cc3e886b8915c
seheonnn <ho78901@naver.com>
2024-03-24 19:14:15 +0900

    ✨  feat: 단기예보 반환값 캐싱 및 RedisUtil 개선

--- weather-service/src/main/java/com/waither/weatherservice/redis/RedisUtil.java
+++ weather-service/src/main/java/com/waither/weatherservice/redis/RedisUtil.java
@@ -8,8 +8,6 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
-import com.waither.weatherservice.entity.ExpectedWeather;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,50 +16,37 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class RedisUtil {
 
-	private static final String EXPECTED_TEMP_SUFFIX = ":expectedTemp";
-	private static final String EXPECTED_RAIN_SUFFIX = ":expectedRain";
-	private static final String EXPECTED_PTY_SUFFIX = ":expectedPty";
-	private static final String EXPECTED_SKY_SUFFIX = ":expectedSky";
 	private final RedisTemplate<String, String> redisTemplate;
 
 	private String convertListToString(List<String> list) {
 		return String.join(",", list);
 	}
 
-	public void saveAsExpectedWeather(String key, ExpectedWeather val, Long timeout, TimeUnit timeUnit) {
+	public void saveAsValue(String key, String val, Long timeout, TimeUnit timeUnit) {
 		redisTemplate.opsForValue()
-			.set(key + EXPECTED_TEMP_SUFFIX, convertListToString(val.getExpectedTemp()), timeout, timeUnit);
+			.set(key, val, timeout, timeUnit);
+	}
+
+	public void saveAsList(String key, List<String> list, Long timeout, TimeUnit timeUnit) {
 		redisTemplate.opsForValue()
-			.set(key + EXPECTED_RAIN_SUFFIX, convertListToString(val.getExpectedRain()), timeout, timeUnit);
-		redisTemplate.opsForValue()
-			.set(key + EXPECTED_PTY_SUFFIX, convertListToString(val.getExpectedPty()), timeout, timeUnit);
-		redisTemplate.opsForValue()
-			.set(key + EXPECTED_SKY_SUFFIX, convertListToString(val.getExpectedSky()), timeout, timeUnit);
+			.set(key, convertListToString(list), timeout, timeUnit);
 	}
 
 	public boolean hasKey(String key) {
 		return Boolean.TRUE.equals(redisTemplate.hasKey(key));
 	}
 
-	public ExpectedWeather getExpectedWeather(String key) {
-		String expectedTemp = Optional.ofNullable(redisTemplate.opsForValue().get(key + EXPECTED_TEMP_SUFFIX)).orElse("");
-		String expectedRain = Optional.ofNullable(redisTemplate.opsForValue().get(key + EXPECTED_RAIN_SUFFIX)).orElse("");
-		String expectedPty = Optional.ofNullable(redisTemplate.opsForValue().get(key + EXPECTED_PTY_SUFFIX)).orElse("");
-		String expectedSky = Optional.ofNullable(redisTemplate.opsForValue().get(key + EXPECTED_SKY_SUFFIX)).orElse("");
+	public String getAsValue(String key) {
+		return Optional.ofNullable(redisTemplate.opsForValue().get(key)).orElse("");
+	}
 
-		return ExpectedWeather.builder()
-			.expectedTemp(Arrays.asList(expectedTemp.split(",")))
-			.expectedRain(Arrays.asList(expectedRain.split(",")))
-			.expectedPty(Arrays.asList(expectedPty.split(",")))
-			.expectedSky(Arrays.asList(expectedSky.split(",")))
-			.build();
+	public List<String> getAsList(String key) {
+		String temp = Optional.ofNullable(redisTemplate.opsForValue().get(key)).orElse("");
+		return Arrays.asList(temp.split(","));
 	}
 
 	public boolean delete(String key) {
-		return redisTemplate.delete(key + EXPECTED_TEMP_SUFFIX) &&
-			redisTemplate.delete(key + EXPECTED_RAIN_SUFFIX) &&
-			redisTemplate.delete(key + EXPECTED_PTY_SUFFIX) &&
-			redisTemplate.delete(key + EXPECTED_SKY_SUFFIX);
+		return redisTemplate.delete(key);
 	}
 }
 
--- weather-service/src/main/java/com/waither/weatherservice/service/WeatherService.java
+++ weather-service/src/main/java/com/waither/weatherservice/service/WeatherService.java
@@ -47,14 +47,20 @@ public class WeatherService {
 		OpenApiResponse.Item item = items.get(0);
 		String key = item.getNx() + "_" + item.getNy() + "_" + item.getFcstDate() + "_" + item.getFcstTime();
 
-		ExpectedWeather expectedWeather = ExpectedWeather.builder()
-			.expectedTemp(expectedTempList)
-			.expectedRain(expectedRainList)
-			.expectedPty(expectedPtyList)
-			.expectedSky(expectedSkyList).build();
+		redisUtil.saveAsList(key + ":expectedTemp", expectedTempList, 6L, TimeUnit.HOURS);
+		redisUtil.saveAsList(key + ":expectedRain", expectedRainList, 6L, TimeUnit.HOURS);
+		redisUtil.saveAsList(key + ":expectedPty", expectedPtyList, 6L, TimeUnit.HOURS);
+		redisUtil.saveAsList(key + ":expectedSky", expectedSkyList, 6L, TimeUnit.HOURS);
 
-		redisUtil.saveAsExpectedWeather(key, expectedWeather, 6L, TimeUnit.HOURS);
-		log.info("ExpectedWeather : {}", redisUtil.getExpectedWeather(key));
+		// TODO 조회 테스트 후 삭제 예정
+		ExpectedWeather expectedWeather = ExpectedWeather.builder()
+			.expectedTemp(redisUtil.getAsList(key + ":expectedTemp"))
+			.expectedRain(redisUtil.getAsList(key + ":expectedRain"))
+			.expectedPty(redisUtil.getAsList(key + ":expectedPty"))
+			.expectedSky(redisUtil.getAsList(key + ":expectedSky"))
+			.build();
+
+		log.info("ExpectedWeather : {}", expectedWeather);
 	}
 
 	public void createDailyWeather(int nx,
@@ -63,7 +69,7 @@ public class WeatherService {
 		String baseTime) throws URISyntaxException {
 
 		// Base_time : 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회)
-		List<OpenApiResponse.Item> items = openApiUtil.callForeCastApi(nx, ny, baseDate, baseTime, 300,
+		List<OpenApiResponse.Item> items = openApiUtil.callForeCastApi(nx, ny, baseDate, baseTime, 350,
 			"http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst");
 
 		String pop = openApiUtil.apiResponseStringFilter(items, "POP");
@@ -73,20 +79,25 @@ public class WeatherService {
 		String vec = openApiUtil.apiResponseStringFilter(items, "VEC");
 		String wsd = openApiUtil.apiResponseStringFilter(items, "WSD");
 
-		log.info(pop);
-		log.info(tmn);
-		log.info(tmx);
-		log.info(reh);
-		log.info(vec);
-		log.info(wsd);
+		OpenApiResponse.Item item = items.get(0);
+		String key = item.getNx() + "_" + item.getNy() + "_" + item.getFcstDate() + "_" + item.getFcstTime();
 
+		redisUtil.saveAsValue(key + "pop", pop, 8L, TimeUnit.HOURS);
+		redisUtil.saveAsValue(key + "tmn", tmn, 8L, TimeUnit.HOURS);
+		redisUtil.saveAsValue(key + "tmx", tmx, 8L, TimeUnit.HOURS);
+		redisUtil.saveAsValue(key + "reh", reh, 8L, TimeUnit.HOURS);
+		redisUtil.saveAsValue(key + "vec", vec, 8L, TimeUnit.HOURS);
+		redisUtil.saveAsValue(key + "wsd", wsd, 8L, TimeUnit.HOURS);
+
+
+		// TODO 조회 테스트 후 삭제 예정
 		DailyWeather dailyWeather = DailyWeather.builder()
-			.pop(pop)
-			.tempMin(tmn)
-			.tempMax(tmx)
-			.humidity(reh)
-			.windVector(vec)
-			.windDegree(wsd)
+			.pop(redisUtil.getAsValue(key + "pop"))
+			.tempMin(redisUtil.getAsValue(key + "tmn"))
+			.tempMax(redisUtil.getAsValue(key + "tmx"))
+			.humidity(redisUtil.getAsValue(key + "reh"))
+			.windVector(redisUtil.getAsValue(key + "vec"))
+			.windDegree(redisUtil.getAsValue(key + "wsd"))
 			.build();
 
 		log.info("{} : ", dailyWeather);
