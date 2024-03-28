4ee9e985ce12acfa90ec65ce780fc736738c95e9
DDonghyeo <gsu9045000@gmail.com>
2024-03-28 22:35:49 +0900

    ♻️refactor : Kafka Consumer Config & Service 변경

---
+++ noti-service/src/main/java/com/waither/notiservice/repository/UserMedianRepository.java
@@ -0,0 +1,7 @@
+package com.waither.notiservice.repository;
+
+import com.waither.notiservice.domain.UserMedian;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserMedianRepository extends JpaRepository<UserMedian, Long> {
+}
