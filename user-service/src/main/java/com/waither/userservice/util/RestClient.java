ab86dfac2777dbad2cf3b066410e40a342b7b401
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-07-18 18:34:43 +0900

    ✨ feature: Weather-Service로 부터 해당 시각의 체감 온도 받아오는 메서드 작성

--- user-service/src/main/java/com/waither/userservice/util/RestClient.java
+++ user-service/src/main/java/com/waither/userservice/util/RestClient.java
@@ -3,6 +3,8 @@ package com.waither.userservice.util;
 import com.waither.userservice.global.exception.CustomException;
 import com.waither.userservice.global.response.ApiResponse;
 import com.waither.userservice.global.response.ErrorCode;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
@@ -12,28 +14,28 @@ import reactor.core.publisher.Mono;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@NoArgsConstructor
 @Component
 public class RestClient {
-    public static final String WEATHER_SERVICE_URL = "http://localhost:8081";
-
-    private final WebClient webClient;
-
-    public RestClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.baseUrl(WEATHER_SERVICE_URL).build();
-    }
+    public static final String WEATHER_SERVICE_URL = "localhost";
 
     public Double getTemperature(double latitude, double longitude, LocalDateTime time) {
-        ApiResponse<String> response = webClient.get()
+        ParameterizedTypeReference<ApiResponse<String>> responseType =
+                new ParameterizedTypeReference<ApiResponse<String>>() {};
+
+        ApiResponse<String> response = WebClient.create(WEATHER_SERVICE_URL)
+                .get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/weather/temperature")
                         .queryParam("latitude", latitude)
                         .queryParam("longitude", longitude)
                         .queryParam("baseTime", time.format(DateTimeFormatter.ISO_DATE_TIME))
+                        .port(8081)
                         .build())
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, clientResponse ->
                         Mono.error(new CustomException(ErrorCode.INTERNAL_SERVER_ERROR_500)))
-                .bodyToMono(new ParameterizedTypeReference<ApiResponse<String>>() {})
+                .bodyToMono(responseType)
                 .block();
 
         if (response != null && "COMMON200".equals(response.getCode())) {
